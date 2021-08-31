ed57c1f1bee8b2137c49f91c6aae49458fd58421
unknown <youssefoualifessuoy@gmail.com>
2021-08-31 19:26:40 +0200

    saveas

--- form/from.js
+++ form/from.js
@@ -17,25 +17,51 @@ function submitform(){
     // data.push(datedenaisance);
     // data.push(lieudenaisance);
     console.log(data);
+    console.log('000000000000000000000000000000000000000');
+    console.log('000000000000000000000000000000000000000');
+    console.log('000000000000000000000000000000000000000');
 
-    url = "https://stagiaire.herokuapp.com/api/stagiaire/store"
+    const url = "https://stagiaire.herokuapp.com/api/stagiaire/store"
     const form = JSON.stringify({Prenom: data[0], Nom: data[1]});
-    var xhr = new XMLHttpRequest();
-xhr.open("POST", url);
+//    xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//     var a;
+//     if (xhttp.readyState === 4 && xhttp.status === 200) {
+//         // Trick for making downloadable link
+//         a = document.createElement('a');
+//         a.href = window.URL.createObjectURL(xhttp.response);
+//         // Give filename you wish to download
+//         a.download = "test-file.xls";
+//         a.style.display = 'none';
+//         document.body.appendChild(a);
+//         a.click();
+//     }
+// };
+// // Post data to URL which handles post request
+// // xhttp.setRequestHeader("Accept", "application/json");
+// // xhttp.setRequestHeader("Content-Type", "application/json");
+// // You should set responseType as blob for binary responses
+// xhttp.responseType = 'blob';
+// xhttp.send(form);
+var xhr = new XMLHttpRequest();
 
+xhr.open("POST", url);
+// xhr.responseType = "blob";
 xhr.setRequestHeader("Accept", "application/json");
 xhr.setRequestHeader("Content-Type", "application/json");
 
 xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
+   var a;
+   if (xhr.readyState === 4 && xhr.status === 200) {
       console.log(xhr.status);
-      console.log(xhr.responseText);
-        
-      window.open(xhr.responseurl);
-        var doc = new jsPDF()
-        doc.text(xhr.responseText, 200, 200)
-        doc.save('test.pdf')
+      console.log(xhr.response);
+    
+   //  var blob = new Blob([xhr.response], {type: "application/pdf;charset=utf-8"});
+   //  console.log(blob);
+
+   //    saveAs(blob, "test.pdf")
+   const pdf = 'https://stagiaire.herokuapp.com/' + xhr.responseText;
+   window.open(pdf);
    }};
    xhr.send(form);
-
 }

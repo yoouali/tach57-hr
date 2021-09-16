6ed61c40b048665a126304f04a6aa382bd8df3c1
unknown <youssefoualifessuoy@gmail.com>
2021-09-16 14:26:39 +0100

    slide aniamtion and check animation

--- entrepreneur/script.js
+++ entrepreneur/script.js
@@ -1,35 +1,98 @@
+//send request
 let formData;
-var entrepreneur = document.getElementById("EntrepreneurForm");
-var loading = document.getElementById("loading")
 const url = "https://stagiaire.herokuapp.com/api/auto-entrepreneur/store";
 let formElement = document.querySelector("form");
+
 formElement.addEventListener('keydown', (e) => {if(e.keyCode === 13){e.preventDefault();}})
 formElement.addEventListener('submit', (e) => {
     e.preventDefault();
+    if (!checkForm())
+        return;
+    moveInAnimation();
     formData = new FormData(formElement);
-    for (var [key, value] of formData.entries()) { console.log(key, value); }
-    // entrepreneur.style.display="none";
-    entrepreneur.classList.add("moveout");
-    // loading.style.display="block";
-    loading.classList.add("movein");
-    var loadingCheck = document.getElementById("loadingCheck");
-    var loadingIcon = document.getElementById("loadingIcon");
+    for (var [key, value] of formData.entries()) {console.log(key, value);}
     let request = new XMLHttpRequest();
     request.open("POST", url);
+    request.setRequestHeader("Accept", "application/json");
     request.onreadystatechange = function () {
-    if(request.readyState === XMLHttpRequest.DONE) {
-    var status = request.status;
-    if (status === 0 || (status >= 200 && status < 400)) {
-    // The request has been completed successfully
-        console.log(request.responseText);
-        if (request.responseText === "true"){loadingCheck.classList.add("check-animation");loadingIcon.style.display="block";}
-        else loadingCheck.classList.add("error-animation");
-    }
-    else {
-    // Oh no! There has been an error with the request!
-        loadingCheck.classList.add("error-animation");
-    }
-    }
+        if(request.readyState === XMLHttpRequest.DONE) {
+            var status = request.status;
+            checkAniamtion(request.responseText);
+            if (status === 0 || (status >= 200 && status < 400)) {
+                // The request has been completed successfully
+                console.log(request.responseText);
+            }
+            else {
+                console.log(request.responseText);
+                // Oh no! There has been an error with the request!
+            }
+        }
     }
     request.send(formData);
 })
+//end request send
+
+
+//check Form
+var formError = document.getElementById("FormError");
+var prenom = document.getElementById("Prenom");
+var nom = document.getElementById("Nom");
+var cin = document.getElementById("CIN");
+var tel = document.getElementById("TEL");
+var email = document.getElementById("Email");
+var dateNaissance = document.getElementById("DateDeNaissance");
+var lieuNaissance = document.getElementById("LieuDeNaissance");
+var identifiant = document.getElementById("Identifiant");
+var dateAdhesion = document.getElementById("DateDadhesion");
+var valableJusquau = document.getElementById("ValableJusquau");
+
+function checkForm(){
+    if (prenom.value == ""){setClassError(prenom, "Prenom");return false;}
+    return true;
+}
+
+// setclassError
+function setClassError(e, filed){
+    formError.innerText = "Ples file " + filed + " filed";
+    setTimeout(function(){formError.innerText = "";}, 3000)
+}
+
+// end check Form
+
+//aniamtion
+var entrepreneur = document.getElementById("EntrepreneurForm");
+var loading = document.getElementById("loading");
+var loadingCheck = document.getElementById("loadingCheck");
+var loadingIcon = document.getElementById("loadingIcon");
+var resultMessage = document.getElementById("ResultMessage");
+
+//slide divs
+function moveInAnimation(){
+    entrepreneur.classList.remove("movein2");
+    entrepreneur.classList.add("moveout");
+    loading.classList.remove("moveout2");
+    loading.classList.add("movein");
+}
+function moveOutAnimation(){
+    entrepreneur.classList.remove("moveout");
+    entrepreneur.classList.add("movein2");
+    loading.classList.remove("movein");
+    loading.classList.add("moveout2");
+}
+
+//reauest check
+function checkAniamtion(e){
+    console.log("the respone text :");
+    console.log(e);
+    if (e === "true"){
+        loadingCheck.classList.add("check-animation");
+        setTimeout(function() {loadingIcon.classList.add("check-icon");}, 800);
+        setTimeout(function() {resultMessage.style.display="block";loading.style.display="none"}, 1600);
+    }
+    else{
+        loadingCheck.classList.add("error-animation");
+        setTimeout(function() {loadingCheck.classList.remove("error-animation");}, 800);
+        setTimeout(function() {moveOutAnimation();;}, 800);
+    }
+}
+//end aniamtion

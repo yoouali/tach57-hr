f67d6aa3fd8d4fcb4e49d10e753514efe4a2059a
unknown <youssefoualifessuoy@gmail.com>
2021-09-20 13:58:36 +0100

    server

--- hr/src/components/Authentications/login.js
+++ hr/src/components/Authentications/login.js
@@ -13,29 +13,37 @@ async function loginUser(credentials) {
 }
 
 export default function Login({ setToken }) {
-  const [username, setUserName] = useState();
+  const [email, setUserName] = useState();
   const [password, setPassword] = useState();
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const token = await loginUser({
-      username,
-      password
-    });
-    setToken(token);
+  const [value, setValue] = useState('');
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+    // alert('A form was submitted: ' + event);
+    console.log(event.target);
+    fetch('https://stagiaire.herokuapp.com/api/login', {
+        method: 'POST',
+        // We convert the React state to JSON and send it as the POST body
+        body: JSON.stringify(event.target)
+      }).then(function(response) {
+        console.log(response)
+        return response.json();
+      });
   }
 
   return(
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form onSubmit={handleSubmit}>
+      <form onChange={} onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" onChange={e => setUserName(e.target.value)} />
+          <input type="email" name="email" id="email"value={value} onChange={handleChange} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" onChange={e => setPassword(e.target.value)} />
+          <input name="password" id="password" type="password"value={value} onChange={handleChange} />
         </label>
         <div>
           <button type="submit">Submit</button>

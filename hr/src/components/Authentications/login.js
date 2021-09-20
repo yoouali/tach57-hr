247158c9cc13835a8d2a7368a4202a75cf48bb04
unknown <youssefoualifessuoy@gmail.com>
2021-09-20 12:51:06 +0100

    server js

--- hr/src/components/Authentications/login.js
+++ hr/src/components/Authentications/login.js
@@ -1,14 +1,50 @@
-function Login(){
-    return(
-        <div>
-            <h1>Login Page</h1>
-            <form>
-                <input name="email" id="email" type="email" placeholder="email"/>
-                <input name="password" id="password" type="password" placeholder="password"/>
-                <button>Login</button>
-            </form>
-        </div>
-    )
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+async function loginUser(credentials) {
+ return fetch('https://stagiaire.herokuapp.com/api/login', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify(credentials)
+ })
+   .then(data => data.json())
 }
 
-export default Login
+export default function Login({ setToken }) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password
+    });
+    setToken(token);
+  }
+
+  return(
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={e => setUserName(e.target.value)} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" onChange={e => setPassword(e.target.value)} />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+};
--- hr/src/components/HOME/index.js
+++ hr/src/components/HOME/index.js
@@ -1,7 +1,15 @@
+import { Redirect  } from "react-router";
+
 function Home(){
+    // const isLogged = !!sessionStorage.getItem('token_object');
+    // if (!isLogged) {
+    //     return (
+    //         <Redirect to="/login" />
+    //     )
+    // }
     return(
         <div>
-            <h1>HOME PAGE</h1>
+            <h1>Home Page</h1>
         </div>
     )
 }

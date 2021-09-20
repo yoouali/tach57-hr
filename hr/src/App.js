f03e352c9f8dcbb734aa44c956ebfb6f9de61863
unknown <youssefoualifessuoy@gmail.com>
2021-09-20 12:02:02 +0100

    authentication

--- hr/src/App.js
+++ hr/src/App.js
@@ -1,13 +1,14 @@
 import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './components/HOME';
+import Login from './components/Authentications/login';
 
 
 function App() {
   return(
     <div className="App">
       <BrowserRouter>
-      <Route path="/"> <Home /> </Route>
-      <Route path="/login"> <Login /> </Route>
+      <Route exact path="/"> <Home /></Route>
+      <Route exact path="/login"> <Login /></Route>
       </BrowserRouter>
     </div>
   );
--- hr/src/components/Authentications/login.js
+++ hr/src/components/Authentications/login.js
@@ -2,6 +2,11 @@ function Login(){
     return(
         <div>
             <h1>Login Page</h1>
+            <form>
+                <input name="email" id="email" type="email" placeholder="email"/>
+                <input name="password" id="password" type="password" placeholder="password"/>
+                <button>Login</button>
+            </form>
         </div>
     )
 }

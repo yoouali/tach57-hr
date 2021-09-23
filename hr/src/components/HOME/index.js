f4db6f5e06cea734ead4cdcf3a60e17ca6b3c6f8
unknown <youssefoualifessuoy@gmail.com>
2021-09-23 13:47:29 +0100

    header

--- hr/src/components/HOME/index.js
+++ hr/src/components/HOME/index.js
@@ -1,23 +1,42 @@
 import axios from "axios";
+import {useState, useContext, useEffect} from 'react';
 import { Redirect  } from "react-router";
 import './style.css';
 
 function Home(){
-    const token = localStorage.getItem('token');
-    const config = {header:{'Authorization': 'Bearer ' + token}};
-    console.log(config)
-    axios.get('https://stagiaire.herokuapp.com/api/user/all', {headers: {"Authorization": `Bearer ${token}`}})
-    .then(res =>{console.log(res);})
-    .catch(err => {console.log("sadsfdsf");console.log(err)})
+    const [isLoading, setLoading] = useState(true);
+    const [user, setUser] = useState(); 
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        const config = {header:{'Authorization': 'Bearer ' + token}};
+        axios.get('https://stagiaire.herokuapp.com/api/user', {headers: {"Authorization": `Bearer ${token}`}})
+        .then(res =>{console.log(res);
+            setUser(res.data);
+            setLoading(false);
+        })
+        .catch(err => {console.log(err)})
+    }, []);
 
+    
     const isLogged = localStorage.getItem('token');
     if (!isLogged || isLogged === undefined) {return (<Redirect to="/login" />)}
+
+    if (isLoading) {
+            return <div className="App">Loading...</div>;
+    }
+
     return(
         <div>
             <section>
                 <div className="header">
-                    <div className="logo"></div>
-                    <div className="user"></div>
+                    <div className="logo">jdfhdskhf</div>
+                    <div className="user">
+                        <span>{user.role}</span>
+                        <ul className="user-nav">
+                            <li>Setting</li>
+                            <li>logout</li>
+                        </ul>
+                    </div>
                 </div>
             </section>
             <h1>Home Page</h1>

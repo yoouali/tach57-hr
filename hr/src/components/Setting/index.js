08b058371c959dc3c8e96f1acd578a918b965bca
unknown <youssefoualifessuoy@gmail.com>
2021-10-23 23:13:29 +0100

    add logout

--- hr/src/components/Setting/index.js
+++ hr/src/components/Setting/index.js
@@ -104,7 +104,7 @@ function Setting(){
                             <li>Profile</li>
                             <Link to="../Setting">
                             <li>Setting</li></Link>
-                            <li className="logout">logout</li>
+                            <Link to="/login"><li  onClick={function(){localStorage.removeItem("token")}} className="logout">logout</li></Link>
                         </ul>
                     </div>
                 </div>

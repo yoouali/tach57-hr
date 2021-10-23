96b4ea83cc607e722b01ca611603e7c6b5054364
unknown <youssefoualifessuoy@gmail.com>
2021-10-23 23:20:04 +0100

    add logout

--- hr/src/components/stagiaireUpdate/index.js
+++ hr/src/components/stagiaireUpdate/index.js
@@ -99,7 +99,7 @@ function StagiaireUpdate(){
                         <li>Profile</li>
                         <Link to="../Setting">
                         <li>Setting</li></Link>
-                        <li className="logout">logout</li>
+                        <Link to="/login"><li  onClick={function(){localStorage.removeItem("token")}} className="logout">logout</li></Link>
                     </ul>
                 </div>
             </div>

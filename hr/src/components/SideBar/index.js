6fff2ef703abf83154ac0808a561dcde2770422b
unknown <youssefoualifessuoy@gmail.com>
2021-11-09 12:07:23 +0100

    inactive user conte

--- hr/src/components/SideBar/index.js
+++ hr/src/components/SideBar/index.js
@@ -11,6 +11,7 @@ function SideBar({user}){
     var entrepreneur = null;
     var stagiaire = null;
     var freelancer = null;
+    var inactive = null;
     if (user.Role === "admin"){
         userList = <Link to="/Userlist">  <div><img src={profileIcon} alt="profileicon" /></div> </Link>;
         callCenter = <Link to="/CallCenterList"><div><img src={freelancerIcon} alt="profileicon" /></div></Link>
@@ -19,16 +20,17 @@ function SideBar({user}){
         entrepreneur = <Link to="/Entrepreneurlist"> <div><img src={entrepreneurIcon} alt="profileicon" /></div> </Link>;
         stagiaire = <Link to="/Stagiairelist">  <div><img src={stagiaireIcon} alt="profileicon" /></div> </Link>;
         freelancer = <Link to="/FreelancerList"><div><img src={freelancerIcon} alt="profileicon" /></div></Link>
-    }       
+    }
+    if (user.Active === 1){
+        inactive = <div className="sideBar"><Link to="/"> <div><img src={profileIcon} alt="profileicon" /></div> </Link>{userList}{entrepreneur}{stagiaire}{freelancer}{callCenter}</div>
+    }
+    if (user.Active === 0){
+        inactive = <div className="sideBar"><div><img src={profileIcon} alt="profileicon" /></div></div>
+    }
     
     return (
-        <div className="sideBar">
-            <Link to="/"> <div><img src={profileIcon} alt="profileicon" /></div> </Link>
-            {userList}
-            {entrepreneur}
-            {stagiaire}
-            {freelancer}
-            {callCenter}
+        <div>
+        {inactive}
         </div>
     )
 }

d25b76874cef9560ce11903a6f9d5c3783804f72
unknown <youssefoualifessuoy@gmail.com>
2021-11-25 04:12:25 +0100

    contact details

--- hr/src/components/Contacts/index.js
+++ hr/src/components/Contacts/index.js
@@ -5,9 +5,16 @@ function Contacts(){
     return(
     <div className="contactsList">
         <div className="contactsListItem">
-            <div className="contactListItemNumber"></div>
-            <div className="contactListItemName"></div>
-            <div className="contactListItemcalls"></div>
+            <div className="contactsListItemOperation">
+                <div className="contactsListItemOperationEdit">...</div>
+                <div className="contactsListItemOperationRemove">&#10007;</div>
+            </div>
+            <div className="contactsListItemName">Youssef Ouali</div>
+            <div className="contactsListItemNumber">0777672016</div>
+            <div className="contactsListItemDetails">
+                <div className="contactsListItemDetailsCalls"> 20</div>
+                <div className="contactsListItemDetailsTime">15.20 min</div>
+            </div>
         </div>
     </div>
     )

6ae3198480e1caee3b4a75653b7cc296b19e8047
unknown <youssefoualifessuoy@gmail.com>
2021-11-25 04:01:48 +0100

    contact build

--- hr/src/components/Contacts/index.js
+++ hr/src/components/Contacts/index.js
@@ -4,6 +4,11 @@ import outgoingCall from '../../images/icons/outgoingcall.png';
 function Contacts(){
     return(
     <div className="contactsList">
+        <div className="contactsListItem">
+            <div className="contactListItemNumber"></div>
+            <div className="contactListItemName"></div>
+            <div className="contactListItemcalls"></div>
+        </div>
     </div>
     )
 }

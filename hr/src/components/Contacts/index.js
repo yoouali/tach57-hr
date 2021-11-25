25d70261f331e04027759c5d6da35b2a16b6910b
unknown <youssefoualifessuoy@gmail.com>
2021-11-25 04:24:16 +0100

    contact styling

--- hr/src/components/Contacts/index.js
+++ hr/src/components/Contacts/index.js
@@ -16,7 +16,32 @@ function Contacts(){
                 <div className="contactsListItemDetailsTime">15.20 min</div>
             </div>
         </div>
+        <div className="contactsListItem">
+            <div className="contactsListItemOperation">
+                <div className="contactsListItemOperationEdit">...</div>
+                <div className="contactsListItemOperationRemove">&#10007;</div>
+            </div>
+            <div className="contactsListItemName">Youssef Ouali</div>
+            <div className="contactsListItemNumber">0777672016</div>
+            <div className="contactsListItemDetails">
+                <div className="contactsListItemDetailsCalls"> 20</div>
+                <div className="contactsListItemDetailsTime">15.20 min</div>
+            </div>
+        </div>
+        <div className="contactsListItem">
+            <div className="contactsListItemOperation">
+                <div className="contactsListItemOperationEdit">...</div>
+                <div className="contactsListItemOperationRemove">&#10007;</div>
+            </div>
+            <div className="contactsListItemName">Youssef Ouali</div>
+            <div className="contactsListItemNumber">0777672016</div>
+            <div className="contactsListItemDetails">
+                <div className="contactsListItemDetailsCalls"> 20</div>
+                <div className="contactsListItemDetailsTime">15.20 min</div>
+            </div>
+        </div>
     </div>
+    
     )
 }
 

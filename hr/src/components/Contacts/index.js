b17f36fb2ab9b9d8b05f0c11cb044dae4073932a
unknown <youssefoualifessuoy@gmail.com>
2021-11-27 17:18:36 +0100

    styling contact

--- hr/src/components/Contacts/index.js
+++ hr/src/components/Contacts/index.js
@@ -6,42 +6,17 @@ function Contacts(){
     <div className="contactsList">
         <div className="contactsListItem">
             <div className="contactsListItemOperation">
-                <div className="contactsListItemOperationEdit">...</div>
-                <div className="contactsListItemOperationRemove">&#10007;</div>
+                <div id="contactListOperationEdit" className="contactsListItemOperationEdit">...</div>
+                <div id="contactListOperationDelete" className="contactsListItemOperationRemove">&#10007;</div>
             </div>
-            <div className="contactsListItemName">Youssef Ouali</div>
-            <div className="contactsListItemNumber">0777672016</div>
+            <div className="contactsListItemName"><p>&#128100;</p>Youssef Ouali</div>
+            <div className="contactsListItemNumber"><p>&#128241;</p>0777672016</div>
             <div className="contactsListItemDetails">
-                <div className="contactsListItemDetailsCalls"> 20</div>
-                <div className="contactsListItemDetailsTime">15.20 min</div>
-            </div>
-        </div>
-        <div className="contactsListItem">
-            <div className="contactsListItemOperation">
-                <div className="contactsListItemOperationEdit">...</div>
-                <div className="contactsListItemOperationRemove">&#10007;</div>
-            </div>
-            <div className="contactsListItemName">Youssef Ouali</div>
-            <div className="contactsListItemNumber">0777672016</div>
-            <div className="contactsListItemDetails">
-                <div className="contactsListItemDetailsCalls"> 20</div>
-                <div className="contactsListItemDetailsTime">15.20 min</div>
-            </div>
-        </div>
-        <div className="contactsListItem">
-            <div className="contactsListItemOperation">
-                <div className="contactsListItemOperationEdit">...</div>
-                <div className="contactsListItemOperationRemove">&#10007;</div>
-            </div>
-            <div className="contactsListItemName">Youssef Ouali</div>
-            <div className="contactsListItemNumber">0777672016</div>
-            <div className="contactsListItemDetails">
-                <div className="contactsListItemDetailsCalls"> 20</div>
-                <div className="contactsListItemDetailsTime">15.20 min</div>
+                <div className="contactsListItemDetailsCalls"><p>&#128222;</p> 20</div>
+                <div className="contactsListItemDetailsTime"><p>&#128336;</p>15.20 min</div>
             </div>
         </div>
     </div>
-    
     )
 }
 

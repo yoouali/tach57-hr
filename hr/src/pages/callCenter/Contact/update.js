f7a01fc43dc05d16ceba0d338d73557af3c20e72
unknown <youssefoualifessuoy@gmail.com>
2021-11-29 04:02:02 +0100

    call component and contact

---
+++ hr/src/pages/callCenter/Contact/update.js
@@ -0,0 +1,14 @@
+import {useState, useEffect} from 'react';
+import { Redirect  } from "react-router";
+import axios from "axios";
+import Header from '../../components/Header';
+import SideBar from '../../components/SideBar';
+
+function Contact(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default Contact

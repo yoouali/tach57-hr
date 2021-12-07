fda2a13006782303b1605a1fc5923cd06202c8be
unknown <youssefoualifessuoy@gmail.com>
2021-12-07 06:23:53 +0100

    call data

--- hr/src/pages/callCenter/Call/index.js
+++ hr/src/pages/callCenter/Call/index.js
@@ -14,6 +14,7 @@ import incomingCall from "../../../images/icons/incomingCall.png";
 import outgoingCall from "../../../images/icons/outgoingCall.png";
 import rejectedCall from "../../../images/icons/rejectedCall.png";
 import missedCall from "../../../images/icons/missedCall.png";
+import clockIcon from "../../../images/icons/clockIcon.png";
 
 function Call() {
   const { id } = useParams();
@@ -39,7 +40,7 @@ function Call() {
         console.log(err);
       });
     axios
-      .get("https://stagiaire.herokuapp.com/api/Client/show/1", {
+      .get("https://stagiaire.herokuapp.com/api/Call/show/1", {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((res) => {
@@ -51,7 +52,9 @@ function Call() {
         console.log(err);
       });
   }, []);
+
   if (!isLoading2 && isLoading3) {
+    setLoading3(false);
     const token = localStorage.getItem("token");
     axios
       .get("https://stagiaire.herokuapp.com/api/Client/" + call.TEL, {
@@ -59,13 +62,48 @@ function Call() {
       })
       .then((res) => {
         console.log(res);
-        setContact(res.data);
-        setLoading3(false);
+        setContact(res.data[0]);
       })
       .catch((err) => {
         console.log(err);
       });
   }
+
+  // COMPONENT FUNCTIONS
+
+  if (!isLoading2) {
+    var callType = null;
+    if (call.CallType === 1)
+      callType = (
+        <div className="callInfoTypeText">
+          <img src={incomingCall} />
+          incoming call
+        </div>
+      );
+    if (call.CallType === 2)
+      callType = (
+        <div className="callInfoTypeText">
+          <img src={outgoingCall} />
+          outgoing call
+        </div>
+      );
+    if (call.CallType === 3)
+      callType = (
+        <div className="callInfoTypeText">
+          <img src={rejectedCall} />
+          rejected call
+        </div>
+      );
+    if (call.CallType === 4)
+      callType = (
+        <div className="callInfoTypeText">
+          <img src={missedCall} />
+          missedcall call
+        </div>
+      );
+  }
+
+  /////////////////////////////////////////////////////////
 
   const isLogged = localStorage.getItem("token");
   if (!isLogged || isLogged === undefined) {
@@ -102,7 +140,46 @@ function Call() {
                   </div>
                 </div>
               </div>
-              <div className="CallInfo"></div>
+              <div className="callInfo">
+                <div className="callInfoDetails">
+                  <div className="callInfoName">OUALI YOUSSEF</div>
+                  {callType}
+                  <div className="callInfoTime">
+                    <img src={clockIcon} />
+                    10.15 min
+                  </div>
+                </div>
+                <div className="callInfoPerss">
+                  <div className="callInfoPerssDoubleGroup">
+                    <div className="callInfoPersDoubleData">
+                      <div className="callInfoPersDataTitle">PHONE NUMBER</div>
+                      <div className="callInfoPersData">call.TEL</div>
+                    </div>
+                    <div className="callInfoPersDoubleData">
+                      <div className="callInfoPersDataTitle">DATE</div>
+                      <div className="callInfoPersData">12-7-2021 6:18</div>
+                    </div>
+                  </div>
+                  <div className="callInfoPerssGroup">
+                    <div className="callInfoPersDataTitle">DESCRIPTION</div>
+                    <div className="callInfoPersData">
+                      lksdfhasjdl lajdljasldj ljal jdaljd ljalj dlajdlajd fd
+                      jksadhksahk kjashdkhk ha dkahskdhaskhdk a
+                      jaksdhkjashdkjhsakjhdksja.
+                    </div>
+                  </div>
+                  <div className="callInfoPerssDoubleGroup">
+                    <div className="callInfoPersDoubleData">
+                      <div className="callInfoPersDataTitle">STTAF</div>
+                      <div className="callInfoPersData">{contact.Staff}</div>
+                    </div>
+                    <div className="callInfoPersDoubleData">
+                      <div className="callInfoPersDataTitle">SOCIETE</div>
+                      <div className="callInfoPersData">{contact.Societe}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
